67b71dcf938d694653d983f7a3553964291f9d5e
Niles Alexis <pl232602@ahschool.com>
2024-01-24 08:48:09 -0500

    intersection between two lines aquired

--- detector.py
+++ detector.py
@@ -2,18 +2,16 @@ import cv2
 import numpy as np 
   
  
-cap = cv2.VideoCapture(r'C:\Users\pl232602\Work Folders\Documents\EDD-Capstone-Project\30 minute Fat Burning Indoor Cycling Workout Alps South Tyrol Lake Tour Garmin 4K Video.mp4') 
-  
+cap = cv2.VideoCapture(r'C:\Users\Niles\OneDrive\Documents\EDD-Capstone-Project\30 minute Fat Burning Indoor Cycling Workout Alps South Tyrol Lake Tour Garmin 4K Video.mp4')
 def avg(input_list):
     try:
-        print(input_list)
-        return sum(input_list)/len(input_list)
+        print(abs(sum(input_list)/len(input_list)))
+        return (sum(input_list)/len(input_list))
     except ZeroDivisionError as error:
         pass
 
-while(cap.isOpened()): 
-      
 
+while True: 
     ret, frame = cap.read() 
     if ret == True: 
         slope_left = []
@@ -36,7 +34,6 @@ while(cap.isOpened()):
 
         h, w, l = frame.shape
 
-        frame = frame[int(h/2):h,0:w]
 
         mask = cv2.inRange(frame, lower_yellow, upper_yellow)
         edges = cv2.Canny(mask, 50, 150, apertureSize=3)
@@ -63,8 +60,9 @@ while(cap.isOpened()):
             
                 y2 = int(y0 - 1000*(a))
                 average_slope = (x2-x1)/(y2-y1)
+                
                 if abs(average_slope) < 2:
-                    cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                    
                     x1s.append(x1)
                     x2s.append(x2)
                     y1s.append(y1)
@@ -74,24 +72,40 @@ while(cap.isOpened()):
                         lx2s.append(x2)
                         ly1s.append(y1)
                         ly2s.append(y2)
-                        alx1s = avg(lx1s)
-                        alx2s = avg(lx2s)
-                        aly1s = avg(ly1s)
-                        aly2s = avg(ly2s)
-                        cv2.line(frame, (alx1s,aly1s), (alx2s,aly2s), (0,0,255), 2)
+                        print("bigger")
+                        
                     elif average_slope < 0:
                         rx1s.append(x1)
                         rx2s.append(x2)
                         ry1s.append(y1)
                         ry2s.append(y2)
-                        arx1s = avg(rx1s)
-                        arx2s = avg(rx2s)
-                        ary1s = avg(ry1s)
-                        ary2s = avg(ry2s)
-                        cv2.line(frame, (arx1s,ary1s), (arx2s,ary2s), (0,0,255), 2)
-                    
+                        print("smaller")
+                        
+            alx1s = int(avg(lx1s))
+            alx2s = int(avg(lx2s))
+            aly1s = int(avg(ly1s))
+            aly2s = int(avg(ly2s))
+            cv2.line(frame, (alx1s,aly1s), (alx2s,aly2s), (0,0,255), 2)
 
-        
+            arx1s = int(avg(rx1s))
+            arx2s = int(avg(rx2s))
+            ary1s = int(avg(ry1s))
+            ary2s = int(avg(ry2s))
+            cv2.line(frame, (arx1s,ary1s), (arx2s,ary2s), (0,0,255), 2)
+
+            m1 = (aly2s - aly1s) / (alx2s - alx1s)
+            b1 = aly1s - m1 * alx1s
+
+            m2 = (ary2s - ary1s) / (arx2s - arx1s)
+            b2 = ary1s - m2 * arx1s
+
+            x_intersection = (b2 - b1) / (m1 - m2)
+
+            y_intersection = m1 * x_intersection + b1
+
+            frame = cv2.circle(frame, (int(x_intersection),int(y_intersection)), radius=0, color=(255, 255, 0), thickness=10)
+
+
 
         except TypeError as e:
             pass
