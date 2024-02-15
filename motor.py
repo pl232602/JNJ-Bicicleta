16216028b8e0af6b77037003b14f61583bc141a7
Niles Alexis <pl232602@ahschool.com>
2024-02-15 08:14:10 -0500

    tryna fix motor

--- motor.py
+++ motor.py
@@ -8,25 +8,20 @@ input_1 = 23
 input_2 = 24
 clock = 25
 
+GPIO.setup(input_1, GPIO.OUT)
+GPIO.setup(input_2, GPIO.OUT)
+GPIO.setup(clock, GPIO.OUT)
+pwm = GPIO.PWM(clock, 1000)
+pwm.start(25)
 
-
-def forward(speed):
-    GPIO.setup(input_1, GPIO.OUT)
-    GPIO.setup(input_2, GPIO.OUT)
-    GPIO.setup(clock, GPIO.OUT)
-    pwm = GPIO.PWM(clock, 1000)
-    pwm.start(25)
+def left(speed):
+    
     pwm.ChangeDutyCycle(speed)
     GPIO.output(input_1,GPIO.HIGH)
     GPIO.output(input_2,GPIO.LOW)
     time.sleep(0.3)
 
-def reverse(speed):
-    GPIO.setup(input_1, GPIO.OUT)
-    GPIO.setup(input_2, GPIO.OUT)
-    GPIO.setup(clock, GPIO.OUT)
-    pwm = GPIO.PWM(clock, 1000)
-    pwm.start(25)
+def right(speed):
     pwm.ChangeDutyCycle(speed)
     GPIO.output(input_1,GPIO.LOW)
     GPIO.output(input_2,GPIO.HIGH)
